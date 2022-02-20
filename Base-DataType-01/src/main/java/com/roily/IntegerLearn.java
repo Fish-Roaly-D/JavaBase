a445ec481c3d446e39cd91e711a8f3a3f6056c05
RoilyFish <1056819225@qq.com>
2022-02-20 15:27:04 +0800

    添加

---
+++ Base-DataType-01/src/main/java/com/roily/IntegerLearn.java
@@ -0,0 +1,20 @@
+package com.roily;
+
+import org.junit.Test;
+
+/**
+ * descripte:
+ * @author: RoilyFish
+ * @date: 2022/2/20
+ */
+public class IntegerLearn {
+    /**
+     * 1、int知识点：装箱拆箱、缓存池技术
+     */
+
+    @Test
+    public void test(){
+        System.out.println(1);
+    }
+
+}
---
+++ Base-DataType-01/src/test/java/test.java
@@ -0,0 +1,18 @@
+import org.junit.Test;
+
+/**
+ * descripte:
+ *
+ * @author: RoilyFish
+ * @date: 2022/2/20
+ */
+public class test {
+
+
+    @Test
+    public void test01(){
+        System.out.println(1);
+    }
+
+
+}
