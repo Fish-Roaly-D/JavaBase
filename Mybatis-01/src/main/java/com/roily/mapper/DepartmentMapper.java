6beb164ced3547a21600cae3712e60aa8ceab939
RoilyFish <1056819225@qq.com>
2022-03-09 00:02:13 +0800

    mybatis更新第一次提交

---
+++ Mybatis-01/src/main/java/com/roily/mapper/DepartmentMapper.java
@@ -0,0 +1,34 @@
+package com.roily.mapper;
+
+import com.roily.entity.Department;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+/**
+ * descripte:
+ *
+ * @author: RoilyFish
+ * @date: 2022/3/8
+ */
+public interface DepartmentMapper {
+    //查询所有记录
+    List<Department> queryBatch();
+
+    //查询所有记录
+    List<Department> queryBatch2();
+
+    //根据id查询
+    Department queryById(@Param("id") Integer id);
+
+    //插入记录
+    int insertIntoDept(Department dept);
+
+    //插入记录
+    Boolean insertIntoDept2(Department dept);
+
+    int deleteById(@Param("id") Integer id);
+
+    int modifyById(Department dept);
+
+}
---
+++ Mybatis-01/src/main/java/com/roily/util/mybatisUtil.java
@@ -0,0 +1,42 @@
+package com.roily.util;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ * descripte:
+ *
+ * @author: RoilyFish
+ * @date: 2022/3/8
+ */
+public class mybatisUtil {
+
+    private static SqlSessionFactory sqlSessionFactory;
+
+    static {
+
+        try {
+            InputStream in = Resources.getResourceAsStream("mybatis-config.xml");
+            //Properties prop = new Properties();
+            //prop.load(in);
+
+            sqlSessionFactory = new SqlSessionFactoryBuilder().build(in);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static SqlSession OpenSqlSession() {
+        return sqlSessionFactory.openSession();
+    }
+
+
+}
