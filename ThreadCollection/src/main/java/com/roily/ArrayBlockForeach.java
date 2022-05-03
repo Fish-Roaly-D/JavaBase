bd8311ce38a5689c02488affb6d66107733fe751
RoilyFish <1056819225@qq.com>
2022-05-04 02:00:29 +0800

    ArrayBlocking Foreach

---
+++ ThreadCollection/src/main/java/com/roily/ArrayBlockForeach.java
@@ -0,0 +1,102 @@
+package com.roily;
+
+import org.junit.Test;
+
+import java.util.Iterator;
+import java.util.Random;
+import java.util.Spliterator;
+import java.util.concurrent.ArrayBlockingQueue;
+
+/**
+ * @version 1.0.0
+ * @Description
+ * @ClassName ArrayBlockForeach.java
+ * @author: RoilyFish
+ * @date: 2022/5/4 1:26
+ */
+public class ArrayBlockForeach {
+
+
+    @Test
+    public void testForeach() throws InterruptedException {
+
+        /**
+         * queue 是 collection的子接口
+         */
+        final StringBuffer sb = new StringBuffer();
+
+        ArrayBlockingQueue<Object> queue = new ArrayBlockingQueue<Object>(20);
+
+        for (int i = 0; i < 20; i++) {
+            queue.add(i);
+        }
+
+        System.out.println("=======增强for循环========");
+        for (Object obj : queue) {
+            sb.append(obj + ", ");
+        }
+        mySout(sb);
+
+        System.out.println("=======Collectio stream api========");
+        queue.stream().forEach((obj) -> {
+            sb.append(obj + ", ");
+        });
+        mySout(sb);
+
+        System.out.println("=======Iterable foreach========");
+        queue.forEach((obj) -> {
+            sb.append(obj + ", ");
+        });
+        mySout(sb);
+
+        System.out.println("=======迭代器========");
+        Iterator<Object> iterator = queue.iterator();
+        while (iterator.hasNext()) {
+            sb.append(iterator.next() + ", ");
+        }
+        mySout(sb);
+
+        System.out.println("=======toArray 转数组foreach========");
+        Object[] objects = queue.toArray();
+        for (int i = 0; i < objects.length; i++) {
+            sb.append(objects[i] + ", ");
+        }
+        mySout(sb);
+
+        //System.out.println("=======take========");
+        //int size = queue.size();
+        //while (size-- > 0) {
+        //    sb.append(queue.take() + ", ");
+        //}
+        //mySout(sb);
+
+        //System.out.println("=======poll  不会await========");
+        //Object poll = null;
+        //while ((poll = queue.poll()) != null) {
+        //    sb.append(poll+", ");
+        //}
+        //mySout(sb);
+
+        //System.out.println("=======分离器1========");
+        //Spliterator<Object> spliterator = queue.spliterator();
+        //while (spliterator.tryAdvance((obj) ->
+        //        sb.append(obj + ", ")
+        //)) ;
+        //mySout(sb);
+
+        System.out.println("=======分离器2========");
+        Spliterator<Object> spliterator2 = queue.spliterator();
+        spliterator2.forEachRemaining((obj) -> sb.append(obj + ", "));
+        mySout(sb);
+    }
+
+    public void mySout(StringBuffer sb) {
+        StringBuffer sbTemp = new StringBuffer("{");
+        sb.deleteCharAt(sb.lastIndexOf(","));
+        sb.append("}");
+        sbTemp.append(sb);
+        System.out.println(sbTemp.toString());
+        sb.delete(0, sb.length());
+    }
+
+}
---
+++ ThreadCollection/src/main/java/com/roily/ArrayBlockGet.java
@@ -0,0 +1,42 @@
+package com.roily;
+
+import org.junit.Test;
+
+import java.util.concurrent.ArrayBlockingQueue;
+
+/**
+ * @version 1.0.0
+ * @Description ABQ获取元素
+ * @ClassName ArrayBlockGet.java
+ * @author: RoilyFish
+ * @date: 2022/5/4 0:05
+ */
+public class ArrayBlockGet {
+
+    @Test
+    public void testPoll() throws InterruptedException {
+
+        ArrayBlockingQueue<Object> queue = new ArrayBlockingQueue<Object>(15);
+
+
+        for (int i = 0; i < 10; i++) {
+            queue.add(i);
+        }
+
+        //peek方法是真正意义上的获取元素
+        System.out.println(queue.peek());
+        System.out.println(queue.peek());
+        System.out.println(queue.peek());
+        System.out.println(queue.peek());
+
+        //poll  和  take 是出队操作  会修改 入队出队指针
+        //poll获取失败 返回null(也就是队列为空)
+        //take会响应中断异常
+        System.out.println(queue.poll());
+        System.out.println(queue.take());
+        System.out.println(queue.take());
+
+    }
+
+
+}
