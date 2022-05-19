c2af513fb1c4a645da75918e4ea7e4c7c6d8e469
RoilyFish <1056819225@qq.com>
2022-05-20 02:58:47 +0800

    HashTable

---
+++ base-map-01/src/main/java/com/roily/map/hashtable/TestHashTableCanAddNullKey.java
@@ -0,0 +1,34 @@
+package com.roily.map.hashtable;
+
+import java.util.Hashtable;
+
+/**
+ * @version 1.0.0
+ * @Description 测试hashTable可不可以添加  null key
+ * @ClassName TestHashTableCanAddNullKey.java
+ * @author: RoilyFish
+ * @date: 2022/5/20 2:09
+ */
+public class TestHashTableCanAddNullKey {
+
+    /**
+     * 不可以添加 空的 key 和 value
+     *
+     * value会检测
+     * key会调用hashCode()方法
+     * @param args
+     */
+
+    public static void main(String[] args) {
+
+        Hashtable<String, String> table = new Hashtable<>();
+
+        //报错  空指针异常
+        table.put(null,"1");
+
+        //报错  空指针异常
+        table.put("zx","");
+    }
+
+
+}
