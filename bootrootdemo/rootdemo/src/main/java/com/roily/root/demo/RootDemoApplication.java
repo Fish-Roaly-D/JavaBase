f308217a5425c8d0b0f2e98113ae7c841c567c25
RoilyFish <1056819225@qq.com>
2022-05-26 23:22:19 +0800

    boot  init

---
+++ bootrootdemo/rootdemo/src/main/java/com/roily/root/demo/RootDemoApplication.java
@@ -0,0 +1,13 @@
+package com.roily.root.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RootDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RootDemoApplication.class, args);
+    }
+
+}
---
+++ bootrootdemo/rootdemo/src/test/java/com/roily/root/demo/RootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roily.root.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RootDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
