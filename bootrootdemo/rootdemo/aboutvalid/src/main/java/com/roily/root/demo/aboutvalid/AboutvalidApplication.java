a3cb360234d575dc9ce2cbb6f8dd10192f1cbb9d
RoilyFish <1056819225@qq.com>
2022-05-29 16:26:02 +0800

    Aboutvalid- 打印注册信息

--- bootrootdemo/rootdemo/aboutvalid/src/main/java/com/roily/root/demo/aboutvalid/AboutvalidApplication.java
+++ bootrootdemo/rootdemo/aboutvalid/src/main/java/com/roily/root/demo/aboutvalid/AboutvalidApplication.java
@@ -2,12 +2,34 @@ package com.roily.root.demo.aboutvalid;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
-@SpringBootApplication(scanBasePackages = {"com.roily.root.demo.common.*","com.roily.root.demo.aboutvalid.*"})
+import javax.annotation.Resource;
+
+@SpringBootApplication(scanBasePackages =
+        {
+                "com.roily.root.demo.common.*",
+                "com.roily.root.demo.aboutvalid.*"
+        })
 public class AboutvalidApplication {
 
+
+    public static ApplicationContext applicationContext;
+
     public static void main(String[] args) {
-        SpringApplication.run(AboutvalidApplication.class, args);
+        applicationContext =
+                SpringApplication.run(AboutvalidApplication.class, args);
+        displayAllBeans();
+    }
+
+    /**
+     * 打印所以装载的bean
+     */
+    public static void displayAllBeans() {
+        String[] allBeanNames = applicationContext.getBeanDefinitionNames();
+        for (String beanName : allBeanNames) {
+            System.err.println(beanName);
+        }
     }
 
 }
