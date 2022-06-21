42930e0924873b3b8397d22ba15e47880d1e3c62
rolyfish <1056819225@qq.com>
2022-06-22 01:32:40 +0800

    finally

--- javabase/src/main/java/com/roily/aboutFinal/Demo01.java
+++ javabase/src/main/java/com/roily/aboutFinal/Demo01.java
@@ -14,8 +14,11 @@ import org.junit.Test;
 public class Demo01 {
 
 
+    /**
+     * 在try  finally代码块之前return不会执行finally代码块。
+     */
     @Test
-    public void test1() {
+    public void testReturnBeforeTry() {
         System.out.println("执行业务");
         if (true) {
             //直接return 通不过编译检查的
@@ -28,8 +31,11 @@ public class Demo01 {
         }
     }
 
+    /**
+     * 在try finally之前结束虚拟机，不会执行finally代码块
+     */
     @Test
-    public void test2() {
+    public void testReturnBeforeExit() {
         System.out.println("执行业务");
         //结束虚拟机
         System.exit(0);
@@ -39,11 +45,44 @@ public class Demo01 {
             System.out.println("执行finally");
         }
     }
-
-    public Integer add() {
+/************关于接本数据类型，return的返回值决定于，表达式的值**************/
+    /**
+     * 后加
+     *
+     * @return
+     */
+    public Integer addNext() {
         Integer i = 10;
         try {
-            return i = i + 100;
+            return i++;
+        } finally {
+            System.out.println("i的值为=>" + i);
+        }
+    }
+
+    /**
+     * 后加
+     *
+     * @return
+     */
+    public Integer addPrev() {
+        Integer i = 10;
+        try {
+            return ++i;
+        } finally {
+            System.out.println("i的值为=>" + ++i);
+        }
+    }
+
+    /**
+     * 表达式
+     *
+     * @return
+     */
+    public Integer addExpression() {
+        Integer i = 10;
+        try {
+            return i + 1;
         } finally {
             System.out.println("i的值为=>" + i);
         }
@@ -52,11 +91,28 @@ public class Demo01 {
     @Test
     public void test3() {
         Demo01 demo01 = new Demo01();
-        System.out.println(demo01.add());
+        System.out.println(demo01.addNext());
+        System.out.println(demo01.addPrev());
+        System.out.println(demo01.addExpression());
     }
 
-    public String str() {
+    /***************finally代码块的执行在return之前***********/
 
+    StringBuffer modifyStringBuffer(StringBuffer sb) {
+        try {
+            return sb.append("return");
+        } finally {
+            sb.append("finally");
+        }
+    }
+
+    @Test
+    public void testFinallyBeforeReturn() {
+        System.out.println(modifyStringBuffer(new StringBuffer("xxx")));
+    }
+
+    /************在final代码块中return，会覆盖之前的return****************/
+    public String returnInFinally() {
         try {
             System.out.println("try执行");
             return "try return";
@@ -68,68 +124,58 @@ public class Demo01 {
 
     @Test
     public void test() {
-
         Demo01 demo01 = new Demo01();
-        System.out.println(demo01.str());
-
+        System.out.println(demo01.returnInFinally());
     }
 
 
-    public int xxx(Integer i) {
+    /*****对于基本数据类型，只有值的概念，return的时候会记住对应值，后续无论在finally中如何修改都不会修改return返回的结果*******/
+    public int finallyModifyInt(int i) {
 
         try {
-            System.out.println("try  原结果=>"+i);
+            System.out.println("try  原结果=>" + i);
             return i;
         } finally {
             if (i >= 16) {
                 i = i & ((1 << 4) - 1);
-                System.out.println("finally 修改后=>"+i);
-            }
-        }
-    }
-    public int xxxxxx(int i) {
-
-        try {
-            System.out.println("try  原结果=>"+i);
-            return i;
-        } finally {
-            if (i >= 16) {
-                i = i & ((1 << 4) - 1);
-                System.out.println("finally 修改后=>"+i);
+                System.out.println("finally 修改后=>" + i);
             }
         }
     }
 
     @Test
-    public void test5(){
+    public void test5() {
         Demo01 demo01 = new Demo01();
-        System.out.println("最终结果=>"+demo01.xxxxxx(20));
-
+        System.out.println("最终结果=>" + demo01.finallyModifyInt(20));
     }
 
+    /******对于不可变的数据类型，修改值就直接修改了引用，而return会记住需要return的引用，所以说不会影响**/
+
+    /****对于可变的引用类型，由于他们1⃣️属于引用类型2⃣️提供了修改当前对象的Api。所以在finally中对对象的修改，会影响return的结果****/
     @Data
     @AllArgsConstructor
-    class Person{
-        int age;
+    class Person {
+        Integer age;
         String name;
     }
-    public Person xxx2(Person p) {
+
+    public Person modifyPerson(Person p) {
 
         try {
-            System.out.println("try  原结果=>"+p);
+            System.out.println("try  原结果=>" + p);
             return p;
         } finally {
             if (p.getName().equals("abc")) {
                 p.setName("123");
-                System.out.println("finally 修改后=>"+p);
+                System.out.println("finally 修改后=>" + p);
             }
         }
     }
 
     @Test
-    public void test52(){
+    public void test52() {
         Demo01 demo01 = new Demo01();
-        System.out.println("最终结果=>"+demo01.xxx2(new Person(23,"abc")));
+        System.out.println("最终结果=>" + demo01.modifyPerson(new Person(23, "abc")));
 
     }
 
