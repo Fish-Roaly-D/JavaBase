bf1e0e7df52057a82897cde3067693f6630f5523
RoilyFish <1056819225@qq.com>
2022-07-25 18:18:00 +0800

    ClassLoader

--- base-datatype-01/src/main/java/com/roily/classloader/ClassLoaderTest.java
+++ base-datatype-01/src/main/java/com/roily/classloader/ClassLoaderTest.java
@@ -1,6 +1,7 @@
 package com.roily.classloader;
 
 //import com.sun.javafx.animation.TickCalculation;
+
 import org.junit.Test;
 
 /**
@@ -11,27 +12,27 @@ import org.junit.Test;
  */
 public class ClassLoaderTest {
     /**
-     * 类的主动引用和被动应用（类的初始化 cinit<>() init<>()）
-     * 创建类的实例
-     * 访问某个类或接口的静态变量，或者对该静态变量赋值
-     * 调用类的静态方法I
-     * 反射（比如：Class.forName（"com.atguigu.Test"））
-     * 初始化一个类的子类
-     * Java虚拟机启动时被标明为启动类的类
-     * JDK7开始提供的动态语言支持：
-     * java.lang.invoke.MethodHandle实例的解析结果REF getStatic、REF putStatic、REF invokeStatic句柄对应的类没有初始化，则初始化
+     * - 类的主动引用和被动应用（类的初始化 cinit<>() init<>()）
+     * -- 创建类的实例
+     * -- 访问某个类或接口的静态变量，或者对该静态变量赋值
+     * -- 调用类的静态方法I
+     * -- 反射（比如：Class.forName（"com.atguigu.Test"））
+     * -- 初始化一个类的子类
+     * -- Java虚拟机启动时被标明为启动类的类
+     * -- JDK7开始提供的动态语言支持：
+     *    java.lang.invoke.MethodHandle实例的解析结果REF getStatic、REF putStatic、REF invokeStatic句柄对应的类没有初始化，则初始化
      */
 
     /**
      * 双亲委派机制
-     *      加载一个类会一层一层的去委派父加载器去加载，（如果加载不了该类）然后再回头
-     *
+     * 加载一个类会一层一层的去委派父加载器去加载，（如果加载不了该类）然后再回头
+     * <p>
      * 沙箱安全机制
-     *      字节码校验器（保证字节码规范）
-     *      双亲委派机制（确保自定义的同类名和java基本类库（优先加载核心类库））
+     * 字节码校验器（保证字节码规范）
+     * 双亲委派机制（确保自定义的同类名和java基本类库（优先加载核心类库））
      */
     @Test
-    public void test01(){
+    public void test01() {
 
         //系统类加载器，又叫app类加载器，就是程序员自己写的java类
         ClassLoader systemClassLoader = ClassLoader.getSystemClassLoader();
