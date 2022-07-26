2de3807649947d4ebc1ad7918d1abf8b856eae62
RoilyFish <1056819225@qq.com>
2022-07-26 18:10:00 +0800

    值传递-自动装箱拆箱

--- base-datatype-01/src/main/java/com/roily/stringandsb/StringAndStringBuilder.java
+++ base-datatype-01/src/main/java/com/roily/stringandsb/StringAndStringBuilder.java
@@ -1,9 +1,12 @@
 package com.roily.stringandsb;
 
 /**
- * descripte: java中只有值传递。基本数据类型就是值、引用数据类型是一份拷贝的引用
- *
- *              String不可变。sb1、sb2调用append可变。
+ * descriptive:
+ * java中只有值传递。
+ * 基本数据类型就是值、引用数据类型的值指的是所指向对象的内存地址
+ * <p>
+ * String不可变。
+ * sb1、sb2调用append可变。一个线程安全、一个非线程安全。
  *
  * @author: RoilyFish
  * @date: 2022/2/22
@@ -13,21 +16,21 @@ public class StringAndStringBuilder {
 
     public static void main(String[] args) {
         String str = "abc";
-        System.out.println("String.class==原字符串=>"+str);
+        System.out.println("String.class==原字符串=>" + str);
         changeStr(str);
-        System.out.println("String.class==修改后=>"+str);
+        System.out.println("String.class==修改后=>" + str);
         System.out.println("===============");
 
         StringBuilder strBuilder = new StringBuilder("abc");
-        System.out.println("SB.class==原字符串=>"+strBuilder);
+        System.out.println("SB.class==原字符串=>" + strBuilder);
         changeStrBuilder(strBuilder);
-        System.out.println("SB.class==修改后=>"+strBuilder);
+        System.out.println("SB.class==修改后=>" + strBuilder);
         System.out.println("===============");
 
         StringBuffer sb = new StringBuffer("abc");
-        System.out.println("SBx.class==原字符串=>"+sb);
+        System.out.println("SBx.class==原字符串=>" + sb);
         changeStrBuffer(sb);
-        System.out.println("SBx.class==修改后=>"+sb);
+        System.out.println("SBx.class==修改后=>" + sb);
         System.out.println("===============");
     }
 
