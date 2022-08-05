88f1eda18edb506dca7e5da291afc99d04168076
RoilyFish <1056819225@qq.com>
2022-08-05 13:44:24 +0800

    jvm01

---
+++ javabase_base/src/main/java/com/roily/jvm/day01/TestVersion.java
@@ -0,0 +1,19 @@
+package com.roily.jvm.day01;
+
+import org.junit.Test;
+
+/**
+ * @Date: 2022/08/04/18:07
+ * @Description:
+ */
+public class TestVersion {
+    
+    @Test
+    public void test(){
+
+        Person person = new Person();
+
+    }
+    
+    
+}
