4cd1b351becec1a0334abdb0d8911472d74e6827
RoilyFish <1056819225@qq.com>
2022-08-22 19:08:21 +0800

    0822

--- javabase_base/src/main/java/com/roily/booknode/javatogod/_02coll/AboutIterable.java
+++ javabase_base/src/main/java/com/roily/booknode/javatogod/_02coll/AboutIterable.java
@@ -4,8 +4,13 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Spliterator;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.stream.Collectors;
 
 /**
  * @Date: 2022/08/09/11:20
@@ -62,5 +67,38 @@ public class AboutIterable {
         spliterator.forEachRemaining(System.out::print);
     }
 
+    /**
+     * CopyOnWriteIterator
+     */
+    @Test
+    public void testCOW(){
+        CopyOnWriteArrayList<String> cowList = new CopyOnWriteArrayList<>(Arrays.asList("1","2","3"));
+
+        Iterator<String> iterator = cowList.iterator();
+        //fail-sfae 集合修改
+        for (String s : cowList) {
+            if ("1".equals(s)){
+                cowList.remove(s);
+            }
+        }
+        //已经修改
+        System.out.println(cowList);
+        //但对迭代器不可见
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+    }
+
+    @Test
+    public void testxxx(){
+        List<String> list = new ArrayList<>(Arrays.asList("1","2","3"));
+        for (String s : list) {
+            if ("1".equals(s)){
+                list.remove(s);
+            }
+            break;
+        }
+        System.out.println(list);
+    }
 
 }
