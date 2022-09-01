d8ae029c380f23e8405df3e5e2ce4b86460ec4af
RoilyFish <1056819225@qq.com>
2022-09-01 16:45:56 +0800

    commit test1

--- javabase_base/src/main/java/com/roily/booknode/javatogod/_08date/TestRollback.java
+++
@@ -1,9 +0,0 @@
-package com.roily.booknode.javatogod._08date;
-
-/**
- * @Date: 2022/09/01/16:19
- * @Description:
- */
-public class TestRollback {
-    //待回滚
-}
--- javabase_base/src/main/java/com/roily/booknode/javatogod/_08date/TestRollback3.java
+++
@@ -1,9 +0,0 @@
-package com.roily.booknode.javatogod._08date;
-
-/**
- * @Date: 2022/09/01/16:19
- * @Description:
- */
-public class TestRollback3 {
-    //确认提交2
-}
