5b2c3adb874315884976548b846c4aee27cfc57c
RoilyFish <1056819225@qq.com>
2022-09-01 16:21:35 +0800

    date

--- javabase_base/src/main/java/com/roily/booknode/javatogod/_08date/TestRollback2.java
+++ javabase_base/src/main/java/com/roily/booknode/javatogod/_08date/TestRollback2.java
@@ -5,4 +5,5 @@ package com.roily.booknode.javatogod._08date;
  * @Description:
  */
 public class TestRollback2 {
+    //修改2
 }
--- javabase_base/src/main/java/com/roily/booknode/javatogod/_08date/TestRollback3.java
+++ javabase_base/src/main/java/com/roily/booknode/javatogod/_08date/TestRollback3.java
@@ -5,4 +5,5 @@ package com.roily.booknode.javatogod._08date;
  * @Description:
  */
 public class TestRollback3 {
+    //修改3
 }
