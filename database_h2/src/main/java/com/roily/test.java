b8b131c2a349571b7ff637a244d4f44e425355ea
RoilyFish <1056819225@qq.com>
2022-09-08 19:03:14 +0800

    0908

--- database_h2/src/main/java/com/roily/test.java
+++ database_h2/src/main/java/com/roily/test.java
@@ -1,8 +1,17 @@
 package com.roily;
 
+import org.junit.Test;
+
 /**
  * @Date: 2022/09/07/18:41
  * @Description:
  */
 public class test {
+    public static void main(String[] args) {
+        System.out.println();
+    }
+    @Test
+    public void test(){
+        System.out.println();
+    }
 }
