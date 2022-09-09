d9ee08a951ddca4972a6a9d4e66d544d9c9c5be5
RoilyFish <1056819225@qq.com>
2022-09-09 18:47:44 +0800

    0909

--- database_h2/src/test/java/com/roily/test.java
+++ database_h2/src/test/java/com/roily/test.java
@@ -3,15 +3,18 @@ package com.roily;
 import org.junit.Test;
 
 /**
- * @Date: 2022/09/07/18:41
+ * @Date: 2022/09/09/14:23
  * @Description:
  */
 public class test {
+
     public static void main(String[] args) {
         System.out.println();
     }
+
     @Test
     public void test(){
         System.out.println();
     }
+
 }
---
+++ javabase_base/src/main/java/com/roily/booknode/javatogod/_11thread/ThreadPriority.java
@@ -0,0 +1,19 @@
+package com.roily.booknode.javatogod._11thread;
+
+import org.junit.Test;
+
+/**
+ * @Date: 2022/09/09/15:42
+ * @Description:
+ */
+public class ThreadPriority {
+
+    @Test
+    public void test(){
+
+        System.out.println(Thread.currentThread().getPriority());
+
+    }
+
+
+}
