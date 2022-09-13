75ee9ff93051d9f73628ef13471ffee081e5275e
rolyfish <1056819225@qq.com>
2022-09-14 01:04:25 +0800

    0913

--- javabase_base/src/main/java/com/roily/booknode/javatogod/_11thread/ThreadPriority.java
+++ javabase_base/src/main/java/com/roily/booknode/javatogod/_11thread/ThreadPriority.java
@@ -9,11 +9,50 @@ import org.junit.Test;
 public class ThreadPriority {
 
     @Test
-    public void test(){
+    public void test() {
+
+        System.out.println(Thread.currentThread().getPriority());
+
+        Thread.currentThread().setPriority(7);
 
         System.out.println(Thread.currentThread().getPriority());
 
     }
 
+    @Test
+    public void test2() {
+        final MyThread myThread1 = new MyThread();
+        final MyThread myThread2 = new MyThread();
+        final MyThread myThread3 = new MyThread();
 
+        final Thread thread1 = new Thread(myThread1);
+        final Thread thread2 = new Thread(myThread2);
+        final Thread thread3 = new Thread(myThread3);
+
+        thread1.start();
+        thread2.start();
+        thread3.start();
+    }
+
+    public static void main(String[] args) {
+        final MyThread myThread1 = new MyThread();
+        final MyThread myThread2 = new MyThread();
+        final MyThread myThread3 = new MyThread();
+
+        final Thread thread1 = new Thread(myThread1);
+        final Thread thread2 = new Thread(myThread2);
+        final Thread thread3 = new Thread(myThread3);
+
+        thread1.start();
+        thread2.start();
+        thread3.start();
+    }
+}
+
+class MyThread implements Runnable {
+
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName() + " running");
+    }
 }
