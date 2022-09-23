2b1689f57edf718fcea30b6bc2475b6da475fac1
RoilyFish <1056819225@qq.com>
2022-09-23 16:21:51 +0800

    0923-01fix aboutStream

--- javabase_base/src/main/java/com/roily/booknode/javatogod/_02coll/AboutStream.java
+++ javabase_base/src/main/java/com/roily/booknode/javatogod/_02coll/AboutStream.java
@@ -1,12 +1,20 @@
 package com.roily.booknode.javatogod._02coll;
 
+import com.google.common.base.Function;
+import org.checkerframework.checker.units.qual.K;
 import org.junit.Test;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import java.util.stream.DoubleStream;
 import java.util.stream.IntStream;
@@ -64,12 +72,9 @@ public class AboutStream {
         System.out.println(s1 + " " + s2);
         System.out.println();
 
+        final List<StringBuilder> sbs = Arrays.asList(new StringBuilder(), new StringBuilder());
         System.out.println("===============对每一个元素进行操作===============");
-        List<String> collect5 = list.stream().peek(ele -> {
-            if (ele.equals("2")) {
-                System.out.println("xxxx");
-            }
-        }).collect(Collectors.toList());
+        List<StringBuilder> collect5 = sbs.stream().peek(ele -> ele.append("123")).collect(Collectors.toList());
         collect5.stream().forEach(System.out::print);
         System.out.println();
 
@@ -165,13 +170,13 @@ public class AboutStream {
         names.add(name2);
         List<Object> collect = people.stream().map(person ->
                 names.stream()
-                .filter(name -> {
-                    return person.id == name.id;
-                })
-                .map(name -> {
-                    person.setName(name.name);
-                    return person;
-                }).collect(Collectors.toList())).flatMap(Collection::stream
+                        .filter(name -> {
+                            return person.id == name.id;
+                        })
+                        .map(name -> {
+                            person.setName(name.name);
+                            return person;
+                        }).collect(Collectors.toList())).flatMap(Collection::stream
         ).collect(Collectors.toList());
 
         System.out.println(collect);
@@ -182,7 +187,7 @@ public class AboutStream {
      * 想要在一个集合内匹配另一个集合信息
      */
     @Test
-    public void testxx(){
+    public void testxx() {
 
         ArrayList<Person> people = new ArrayList<>();
         ArrayList<Name> names = new ArrayList<>();
@@ -206,11 +211,10 @@ public class AboutStream {
         names.add(name2);
 
         final HashMap<Object, Object> collect = names.stream().collect(HashMap::new, (map, name) -> {
-            map.put(name.getId(),name);
+            map.put(name.getId(), name);
         }, (r1, r2) -> {
             System.out.println(r1);
         });
-
 
 
         System.out.println(collect);
@@ -220,15 +224,100 @@ public class AboutStream {
 
 
     @Test
-    public void eeee(){
+    public void eeee() {
 
-        String str = "123"+"abc";
+        String str = "123" + "abc";
         String str2 = "123x";
         String str3 = "abcx";
 
     }
-    void append(String str1 ,String str2){
+
+    void append(String str1, String str2) {
         String str3 = str1 + str2;
     }
+
+
+    /**
+     * 将多个Stream合并成一个Stream
+     */
+    @Test
+    public void testFlatMap() {
+
+        final List<List<Integer>> lists = Arrays.asList(Arrays.asList(1, 2), Arrays.asList(1, 2));
+
+        final List<Integer> collect = lists.stream().flatMap(Collection::stream).collect(Collectors.toList());
+
+        collect.forEach(System.out::print);
+
+    }
+
+    /**
+     * 将多个Stream合并成一个Stream
+     */
+    @Test
+    public void testFlatMaps() {
+        final List<Integer> list1 = Arrays.asList(1, 2, 3);
+        final List<String> list2 = Arrays.asList("1", "2", "3");
+        final List<? extends List<? extends Serializable>> lists = Arrays.asList(list1, list2);
+        final List<String> collect = lists.stream().flatMap(list ->
+                list.stream().map(Object::toString)
+        ).collect(Collectors.toList());
+        collect.stream().forEach(System.out::print);
+    }
+
+
+    @Test
+    public void testReduce() {
+        final List<Integer> list1 = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 0);
+
+        //对集合进行聚合求值，e1首次为集合第一个参数，之后为return结果，e2为 2、3.。。。。
+        final Optional<Integer> reduce1 = list1.stream().reduce(Integer::sum);
+        System.out.println(reduce1.get());
+
+        //reduce的重载，返回结果为T，e1首个为100
+        final Integer reduce2 = list1.stream().reduce(100, Integer::sum);
+        System.out.println(reduce2);
+
+        //非并行流，只有一个线程，第三个参数BinaryOperator，可以忽略
+        final StringBuilder reduce3 = list1.stream().reduce(new StringBuilder(), StringBuilder::append, (t, u) -> {
+            int i = 0;
+            return t;
+        });
+        System.out.println("非并行流" + reduce3);
+        // final StringBuilder reduce4 = list1.parallelStream().reduce(new StringBuilder(), StringBuilder::append, (t, u) -> {
+        //     u.append(t);
+        //     return u;
+        // });
+        // System.out.println("并行流" + reduce4);
+
+
+        final HashMap<String, Integer> map1 = list1.stream().collect(HashMap::new, (mapx, ele) -> {
+            mapx.put(ele.toString(), ele);
+        }, (a1, a2) -> {
+        });
+        System.out.println("非并行流，未addall" + map1);
+
+        /**
+         * 三个参数
+         * Supplier 供应商，提供返回对象
+         * BiConsumer  消费者  操作返回对象和流中的元素
+         * BiConsumer  消费者  对并行流结果进行操作
+         */
+        final HashMap<String, Integer> map2 = list1.parallelStream().collect(HashMap::new, (mapx, ele) -> {
+            mapx.put(ele.toString(), ele);
+        }, (mapa, mapb) -> {
+        });
+
+        System.out.println("并行流，未addall" + map2);
+
+        final HashMap<String, Integer> map3 = list1.parallelStream().collect(HashMap::new, (mapx, ele) -> {
+            mapx.put(ele.toString(), ele);
+        }, HashMap::putAll);
+        System.out.println("并行流，addall" + map3);
+
+
+
+    }
+
 
 }
---
+++ javabase_base/src/main/java/com/roily/booknode/javatogod/_12hanshubianc/HanShu.java
@@ -0,0 +1,71 @@
+package com.roily.booknode.javatogod._12hanshubianc;
+
+import com.google.common.cache.RemovalListener;
+import org.checkerframework.checker.units.qual.K;
+import org.junit.Test;
+
+import java.util.function.BiConsumer;
+import java.util.function.BiFunction;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+
+/**
+ * @Date: 2022/09/23/14:46
+ * @Description:
+ */
+public class HanShu {
+
+
+    @Test
+    public void test1() {
+
+        Consumer<Person> method1x = (person) -> {
+        };
+        //简单方法引用，无参数
+        final Consumer<Person> method1 = Person::method1;
+
+
+        final BiConsumer<Person, String> method2x = (person, str) -> {
+            person.method2(str);
+        };
+        //一个参数方法，范型：第一个调用方，第二个方法参数
+        final BiConsumer<Person, String> method2 = Person::method2;
+
+
+
+        MyDemoI<Person,String,String> myDemoI = Person::method2;
+
+        myDemoI.method(new Person(),"","");
+
+        final Person person = new Person();
+        final BiConsumer<String, String> stringStringBiConsumer = person::method2;
+
+
+        Predicate<String> predicate1 =  (String ele) -> ele.startsWith("");
+        Predicate<String> predicate2 =  (String ele) -> ele.startsWith("");
+        predicate1.and(predicate2);
+    }
+
+
+}
+
+class Person {
+
+    public void method1() {
+        System.out.println();
+    }
+
+    public void method2(String str1) {
+        System.out.println("method2(String str1)");
+    }
+
+    public void method2(String str1, String str2) {
+        System.out.println("method2(String str1, String str2)");
+    }
+}
+
+interface MyDemoI<T, K, V> {
+
+    void method(T t, K k, V v);
+
+}
