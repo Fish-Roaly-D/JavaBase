758346607ce340e6f19300347bf2f5b62fb06807
RoilyFish <1056819225@qq.com>
2022-09-28 10:43:05 +0800

    fix

--- spring-helloworld-01/src/main/java/com/roily/demo.java
+++ spring-helloworld-01/src/main/java/com/roily/demo.java
@@ -19,14 +19,14 @@ public class demo {
 
     public static void main(String[] args) {
 
-        // final ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
-        // final UserService userService = applicationContext.getBean("userServiceImpl", UserService.class);
-        // final List<User> all = userService.getAll();
-        // System.out.println(all);
-
-        final AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(ApplicationASConfig.class);
-        final UserService userService = ac.getBean("userServiceImpl", UserService.class);
+        final ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+        final UserService userService = applicationContext.getBean("userServiceImpl", UserService.class);
         final List<User> all = userService.getAll();
         System.out.println(all);
+
+        // final AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(ApplicationASConfig.class);
+        // final UserService userService = ac.getBean("userServiceImpl", UserService.class);
+        // final List<User> all = userService.getAll();
+        // System.out.println(all);
     }
 }
