7f82a3967170fa6aac29e5e55800a17021c4aa35
rolyfish <1056819225@qq.com>
2022-11-16 01:47:44 +0800

    如何创建web项目

--- boot/rootdemo/aboutaop/src/main/java/com/roily/root/demo/aboutaop/entity/User.java
+++ boot/rootdemo/aboutaop/src/main/java/com/roily/root/demo/aboutaop/entity/User.java
@@ -8,12 +8,13 @@ import com.baomidou.mybatisplus.annotation.TableLogic;
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.baomidou.mybatisplus.annotation.Version;
 import com.baomidou.mybatisplus.extension.activerecord.Model;
-import java.io.Serializable;
-import java.time.LocalDateTime;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
 
 /**
  * <p>
@@ -39,30 +40,30 @@ public class User extends Model<User> {
     @TableField("name")
     private String name;
 
-    //@ApiModelProperty("邮箱")
-    //@TableField("email")
-    //private String email;
-    //
-    //@ApiModelProperty("逻辑删除")
-    //@TableField("deleted")
-    //@TableLogic
-    //private Integer deleted;
-    //
-    //@ApiModelProperty("乐观锁")
-    //@TableField("version")
-    //@Version
-    //private Integer version;
-    //
-    //@ApiModelProperty("插入时间")
-    //@TableField(value = "create_time", fill = FieldFill.INSERT)
-    //private LocalDateTime createTime;
-    //
-    //@ApiModelProperty("修改时间")
-    //@TableField(value = "update_time", fill = FieldFill.INSERT_UPDATE)
-    //private LocalDateTime updateTime;
-    //
-    //@TableField("tb_user_test_colum")
-    //private String tbUserTestColum;
+    @ApiModelProperty("邮箱")
+    @TableField("email")
+    private String email;
+
+    @ApiModelProperty("逻辑删除")
+    @TableField("deleted")
+    @TableLogic
+    private Integer deleted;
+
+    @ApiModelProperty("乐观锁")
+    @TableField("version")
+    @Version
+    private Integer version;
+
+    @ApiModelProperty("插入时间")
+    @TableField(value = "create_time", fill = FieldFill.INSERT)
+    private LocalDateTime createTime;
+
+    @ApiModelProperty("修改时间")
+    @TableField(value = "update_time", fill = FieldFill.INSERT_UPDATE)
+    private LocalDateTime updateTime;
+
+    @TableField("tb_user_test_colum")
+    private String tbUserTestColum;
 
 
     @Override
