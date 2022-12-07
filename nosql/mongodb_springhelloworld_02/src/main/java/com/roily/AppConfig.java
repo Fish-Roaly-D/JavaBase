47ade980d1697ac29d910bc9cf0818d2edde93a1
RoilyFish <1056819225@qq.com>
2022-12-07 18:34:05 +0800

    mongodb-driver

--- nosql/mongodb_springhelloworld_02/src/main/java/com/roily/AppConfig.java
+++ nosql/mongodb_springhelloworld_02/src/main/java/com/roily/AppConfig.java
@@ -1,18 +1,18 @@
-package com.roily;
-
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoClients;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class AppConfig {
-
-  /*
-   * Use the standard Mongo driver API to create a com.mongodb.client.MongoClient instance.
-   */
-   public @Bean MongoClient mongoClient() {
-       return MongoClients.create("mongodb://localhost:27017");
-   }
-
-}
+// package com.roily;
+//
+// import com.mongodb.client.MongoClient;
+// import com.mongodb.client.MongoClients;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+//
+// @Configuration
+// public class AppConfig {
+//
+//   /*
+//    * Use the standard Mongo driver API to create a com.mongodb.client.MongoClient instance.
+//    */
+//    public @Bean MongoClient mongoClient() {
+//        return MongoClients.create("mongodb://localhost:27017");
+//    }
+//
+// }
--- nosql/mongodb_springhelloworld_02/src/main/java/com/roily/basetemplate/MongoApp.java
+++ nosql/mongodb_springhelloworld_02/src/main/java/com/roily/basetemplate/MongoApp.java
@@ -1,7 +1,5 @@
 package com.roily.basetemplate;
 
-import static org.springframework.data.mongodb.core.query.Criteria.where;
-
 import com.mongodb.client.MongoClients;
 import com.roily.entity.Address;
 import com.roily.entity.Person;
@@ -12,13 +10,19 @@ import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Query;
 
+import java.util.Collection;
+import java.util.Collections;
+
+import static org.springframework.data.mongodb.core.query.Criteria.where;
+
 public class MongoApp {
 
     private static final Log log = LogFactory.getLog(MongoApp.class);
 
     public static void main(String[] args) throws Exception {
 
-        MongoOperations mongoOps = new MongoTemplate(MongoClients.create(), "test");
+
+        MongoOperations mongoOps = new MongoTemplate(MongoClients.create(),"test");
 
         final Person person = new Person();
         person.setId(new ObjectId());
@@ -26,7 +30,9 @@ public class MongoApp {
         person.setName("person4");
         person.setAddress(new Address("长桥街道", "上海", "10010"));
 
-        mongoOps.insert(person,"person");
+        final Collection<Person> person1 = mongoOps.insert(Collections.singletonList(person), "person");
+
+        System.out.println(person1);
 
 
         System.out.println(mongoOps.findOne(new Query(where("name").is("person1")), Person.class, "person"));
