fdf9e515793fa7c6534ece55708b67d7d9ebb3fd
rolyfish <1056819225@qq.com>
2023-02-14 23:21:34 +0800

    redisTemplate-demo

--- nosql/redis_jedis_01/src/main/java/com/roily/util/JedisFactory.java
+++ nosql/redis_jedis_01/src/main/java/com/roily/util/JedisFactory.java
@@ -5,6 +5,8 @@ import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 
+import java.time.Duration;
+
 /**
  * @author: rolyfish
  */
@@ -13,15 +15,24 @@ public class JedisFactory {
 
     static {
         final GenericObjectPoolConfig<Jedis> config = new GenericObjectPoolConfig<>();
-        config.setMaxTotal(1024);
+        // 最大连接数
+        config.setMaxTotal(10);
+        // 最大空闲连接
         config.setMaxIdle(10);
+        // 最小空闲连接
+        config.setMinIdle(1);
+        // 等待时长
+        config.setMaxWait(Duration.ofMillis(2000));
+        // 从连接池得到连接前会进行校验，校验不通过则销毁当前连接，并借用一个新的连接
         config.setTestOnBorrow(true);
+        // 返回结果前对链接进行校验，校验不通过则销毁当前连接
         config.setTestOnReturn(true);
         pool = new JedisPool(config, "10.211.55.4", 6379, 6000, "123123");
     }
 
     /**
      * 获取jedis实例
+     *
      * @return
      */
     public static Jedis jedis() {
@@ -30,6 +41,7 @@ public class JedisFactory {
 
     /**
      * 关闭jedis资源
+     *
      * @param jedis
      */
     public static void close(Jedis jedis) {
