ad95220029c246cde76b5a6815916c1cc5737b41
rolyfish <1056819225@qq.com>
2023-02-28 01:52:33 +0800

    0228

--- javabase/javabase_base/src/main/java/com/roily/booknode/javatogod/_11thread/threadLocal/SessionManager.java
+++ javabase/javabase_base/src/main/java/com/roily/booknode/javatogod/_11thread/threadLocal/SessionManager.java
@@ -2,6 +2,7 @@ package com.roily.booknode.javatogod._11thread.threadLocal;
 
 import cn.hutool.db.Session;
 import lombok.Data;
+import org.junit.Assert;
 
 /**
  * @author: rolyfish
@@ -17,5 +18,19 @@ public class SessionManager {
     //    }
     //    return s;
     //}
+
+
+    public static void main(String[] args) {
+
+        final ClassLoader app = SessionManager.class.getClassLoader();
+        final ClassLoader ext = app.getParent();
+        final ClassLoader boot = ext.getParent();
+        System.out.println(app);
+        System.out.println(ext);
+        System.out.println(boot);
+
+        final ClassLoader app1 = String.class.getClassLoader();
+        System.out.println(app1);
+    }
 
 }
